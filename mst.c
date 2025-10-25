b7a673b5313190b74a82a39364169d979bc23502
eshwar-2006 <kathrotheeshwar@gmail.com>
2025-10-25 12:32:16 +0530

    Create mst.c

---
+++ mst.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Your MST code here
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    // Read nodes and edges
+    // Compute MST
+    printf("MST weight: %d\n", 42); // Example output
+    return 0;
+}
