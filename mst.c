99f394725ed5aed94c75fd8b03bebb45d903902e
eshwar-2006 <kathrotheeshwar@gmail.com>
2025-10-25 12:41:10 +0530

    Update mst.c

--- mst.c
+++ mst.c
@@ -2,10 +2,21 @@
 // Your MST code here
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    // Read nodes and edges
-    // Compute MST
-    printf("MST weight: %d\n", 42); // Example output
+    int mst_weight = 0;
+int included[MAX] = {0};
+
+for(int i=0; i<m; i++){
+    int u = edges[i].u;
+    int v = edges[i].v;
+    
+    if(find(u) != find(v)){
+        union_set(u,v);
+        mst_weight += edges[i].w;
+        if(!included[u]) { mst_weight += node_weight[u]; included[u]=1; }
+        if(!included[v]) { mst_weight += node_weight[v]; included[v]=1; }
+    }
+}
+
+printf("%d\n", mst_weight);
     return 0;
 }
