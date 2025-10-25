9f5c9bc02fd9fae10af25e95ec025c81d1b10f85
eshwar-2006 <kathrotheeshwar@gmail.com>
2025-10-25 14:16:12 +0530

    Refactor input handling and simplify error checks

--- mst.c
+++ mst.c
@@ -34,33 +34,14 @@ void unite(int a, int b, int* parent, int* rank) {
 
 int main() {
     int n, m;
-    if (scanf("%d %d", &n, &m) != 2) {
-        fprintf(stderr, "Invalid input\n");
-        return 1;
-    }
+    if (scanf("%d %d", &n, &m) != 2) return 1;
 
-    // Read node weights
     int* node_weight = (int*)malloc((n + 1) * sizeof(int));
-    for (int i = 1; i <= n; i++) {
-        if (scanf("%d", &node_weight[i]) != 1) {
-            fprintf(stderr, "Invalid node weight\n");
-            free(node_weight);
-            return 1;
-        }
-    }
+    for (int i = 1; i <= n; i++) scanf("%d", &node_weight[i]);
 
-    // Read edges
     Edge* edges = (Edge*)malloc(m * sizeof(Edge));
-    for (int i = 0; i < m; i++) {
-        if (scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w) != 3) {
-            fprintf(stderr, "Invalid edge data\n");
-            free(node_weight);
-            free(edges);
-            return 1;
-        }
-    }
+    for (int i = 0; i < m; i++) scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 
-    // Initialize Union-Find
     int* parent = (int*)malloc((n + 1) * sizeof(int));
     int* rank = (int*)malloc((n + 1) * sizeof(int));
     int* included = (int*)calloc(n + 1, sizeof(int));
@@ -70,17 +51,35 @@ int main() {
         rank[i] = 0;
     }
 
-    // Sort edges by weight
     qsort(edges, m, sizeof(Edge), cmp_edges);
 
     long long mst_weight = 0;
     int edges_used = 0;
 
-    // Kruskal's algorithm
     for (int i = 0; i < m && edges_used < n - 1; i++) {
         int u = edges[i].u;
-        int v = edg
-fflush(stdout);
-printf("%lld\n", mst_weight);
-fflush(stdout);
+        int v = edges[i].v;
+        int w = edges[i].w;
 
+        int pu = find(u, parent);
+        int pv = find(v, parent);
+        if (pu != pv) {
+            mst_weight += w;
+            if (!included[u]) { mst_weight += node_weight[u]; included[u] = 1; }
+            if (!included[v]) { mst_weight += node_weight[v]; included[v] = 1; }
+            unite(pu, pv, parent, rank);
+            edges_used++;
+        }
+    }
+
+    printf("%lld\n", mst_weight);
+    fflush(stdout);
+
+    free(node_weight);
+    free(edges);
+    free(parent);
+    free(rank);
+    free(included);
+
+    return 0;
+}
