837f3d2c896f8de0aa13dfbfed7074222a715c4a
eshwar-2006 <kathrotheeshwar@gmail.com>
2025-10-25 13:47:43 +0530

    Refactor MST implementation in mst.c
    Refactor MST algorithm to improve readability and error handling.

--- mst.c
+++ mst.c
@@ -1,66 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100
-
 typedef struct {
-    int u, v, w;
+    int u, v;
+    int w;
 } Edge;
 
-int parent[MAX];
-int node_weight[MAX];
-int included[MAX];
+int cmp_edges(const void* a, const void* b) {
+    Edge* ea = (Edge*)a;
+    Edge* eb = (Edge*)b;
+    return ea->w - eb->w;
+}
 
-int find(int x) {
-    if(parent[x] != x)
-        parent[x] = find(parent[x]);
+int find(int x, int* parent) {
+    if (parent[x] != x) parent[x] = find(parent[x], parent);
     return parent[x];
 }
 
-void union_set(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if(a != b) parent[b] = a;
+void unite(int a, int b, int* parent, int* rank) {
+    int pa = find(a, parent);
+    int pb = find(b, parent);
+    if (pa == pb) return;
+    if (rank[pa] < rank[pb]) {
+        parent[pa] = pb;
+    } else if (rank[pa] > rank[pb]) {
+        parent[pb] = pa;
+    } else {
+        parent[pb] = pa;
+        rank[pa]++;
+    }
 }
 
-int cmp(const void *a, const void *b) {
-    return ((Edge*)a)->w - ((Edge*)b)->w;
-}
-
-int main() {
+int main(void) {
     int n, m;
-    scanf("%d %d", &n, &m);
-
-    for(int i=1; i<=n; i++){
-        parent[i] = i;
-        included[i] = 0;
+    if (scanf("%d %d", &n, &m) != 2) {
+        fprintf(stderr, "Invalid input: missing n m\\n");
+        return 1;
     }
 
-    // Read node weights separately
-    for(int i=1; i<=n; i++){
-        scanf("%d", &node_weight[i]);
-    }
-
-    Edge edges[m];
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
-    }
-
-    qsort(edges, m, sizeof(Edge), cmp);
-
-    int mst_weight = 0;
-
-    for(int i=0; i<m; i++){
-        int u = edges[i].u;
-        int v = edges[i].v;
-        if(find(u) != find(v)){
-            union_set(u,v);
-            mst_weight += edges[i].w;
-            if(!included[u]) { mst_weight += node_weight[u]; included[u]=1; }
-            if(!included[v]) { mst_weight += node_weight[v]; included[v]=1; }
+    int* node_weight = (int*)malloc((n + 1) * sizeof(int));
+    for (int i = 1; i <= n; i++) {
+        if (scanf("%d", &node_weight[i]) != 1) {
+            fprintf(stderr, "Invalid input: missing node weights\\n");
+            free(node_weight);
+            return 1;
         }
     }
 
-    printf("%d\n", mst_weight);
-    return 0;
-}
+    Edge* edges = (Edge*)malloc(m * sizeof(Edge));
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        if (scanf("%d %d %d", &u, &v, &w) != 3) {
+            fprintf(stderr, "Invalid input: missing edge data\\n");
+            free(edges);
+            free(node_weight);
+            return 1;
+        }
+        edges[i].u = u;
+        edges[i].v = v;
+        edges[i].w = w;
+    }
+
+    // Kruskal prep
+    int* parent = (int*)malloc((n + 1) * sizeof(int));
+    int* rank = (int*)malloc((n + 1) * sizeof(int));
+    int* included = (int*)calloc(n + 1, sizeof(int)); // 0 or 1
+
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+
+    qsort(edges, m, sizeof(Edge), cmp_edges);
+
+    long long total = 0;
+
+    int edges_used = 0;
+    for (int i = 0; i < m && edges_used < n - 1; i++) {
+        int u = edges[i].
